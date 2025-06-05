8fb200e92f034457fcdc8eaa7fadee85d2e4f053
pmoua001 <pmoua8906@gmail.com>
2025-06-05 00:13:32 -0500

    Add files via upload

---
+++ Covid Practice.sql
@@ -0,0 +1,20 @@
+--Finding the amount of cases and deaths per state
+SELECT
+	covid.state AS states_affected,
+	SUM(cases) AS cases_per_state,
+	SUM(deaths) AS deaths_per_state,
+	us_population._2020_census AS population_num,
+	AVG(deaths) AS avg_deaths_per_state,
+	AVG(cases) AS avg_cases_per_state
+FROM 
+	covid INNER JOIN us_pop_by_state AS us_population
+	ON covid.state = us_population.state
+--Only wanted the 50 states, covid table had 55 states listed
+GROUP BY
+	covid.state, 
+	us_population._2020_census
+--Grouped by state total numbers of deaths and cases 
+--Grouped by census to help group other data
+ORDER BY 
+	cases_per_state DESC, deaths_per_state DESC
+--Finding the highest case and deaths by state
